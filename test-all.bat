1547e942aa4d5b292b6b006f0305ae33b7ef23ea
Benji Smith <smith.benj@husky.neu.edu>
2016-03-31 19:05:36 -0400

    Windows cygwin test running.

---
+++ test-all.bat
@@ -0,0 +1,19 @@
+@echo off
+
+g++ -Wall -Wextra -std=c++11 -DBNS_DEBUG -DEXIT_ON_ASSERT -DREF_TEST_MAIN ref.cpp -o ref_test
+if errorlevel 1 goto somethingbad
+
+ref_test.exe
+if errorlevel 1 goto somethingbad
+
+g++ -Wall -Wextra -std=c++11 -DBNS_DEBUG -DASSERT_TEST_MAIN assert.cpp -o assert_test
+if errorlevel 1 goto somethingbad
+
+assert_test.exe < b-all.txt
+if errorlevel 1 goto somethingbad
+
+echo Success!
+goto EOF
+:somethingbad
+echo Something Bad Happened.
+:EOF
