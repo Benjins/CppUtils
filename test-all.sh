d9691ca8a6ba5456f81b2f1443a6bd66ec140759
Benji Smith <smith.benj@husky.neu.edu>
2016-04-08 20:02:30 -0400

    Re-enabling tests in test-all.bat, adding stringmap test to test-all.sh.

--- test-all.sh
+++ test-all.sh
@@ -27,4 +27,7 @@ valgrind --quiet --leak-check=full --error-exitcode=12 ./idbase_test
 eval "$CXX -Wall -Wextra -g -std=c++11 -DBNS_DEBUG -DEXIT_ON_ASSERT -DSTRINGS_TEST_MAIN assert.cpp strings.cpp -o strings_test"
 valgrind --quiet --leak-check=full --error-exitcode=12 ./strings_test
 
-cppcheck --force --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction --error-exitcode=12 --template '{file},{line},{severity},{id},{message}' --quiet --verbose -DBNS_DEBUG -UREF_TEST_MAIN -UASSERT_TEST_MAIN -UHASH_TEST_MAIN -UVECTOR_TEST_MAIN -UFILESYS_TEST_MAIN -UIDBASE_TEST_MAIN -USTRINGS_TEST_MAIN --enable=all --std=c++11 *.cpp
+eval "$CXX -Wall -Wextra -g -std=c++11 -DBNS_DEBUG -DEXIT_ON_ASSERT -DSTRINGMAP_TEST_MAIN assert.cpp strings.cpp hash.cpp stringmap.cpp -o stringmap_test"
+valgrind --quiet --leak-check=full --error-exitcode=12 ./stringmap_test
+
+cppcheck --force --inline-suppr --suppress=missingIncludeSystem --suppress=unusedFunction --error-exitcode=12 --template '{file},{line},{severity},{id},{message}' --quiet --verbose -DBNS_DEBUG -UREF_TEST_MAIN -UASSERT_TEST_MAIN -UHASH_TEST_MAIN -UVECTOR_TEST_MAIN -UFILESYS_TEST_MAIN -UIDBASE_TEST_MAIN -USTRINGS_TEST_MAIN -USTRINGMAP_TEST_MAIN --enable=all --std=c++11 *.cpp
